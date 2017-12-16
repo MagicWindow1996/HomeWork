55bfc2d07120e17a5566af9bb046feb0c0948939
Linkincc <1751289712@qq.com>
2017-12-16 10:08:35 +0800

    seajs

--- resource/seajs/mod1.js
+++ resource/seajs/mod1.js
@@ -1,13 +1,25 @@
 //sea 的模块化必须包含在一个define 中
 define( function(require, exprots,module) {
-    //exprots
+    //exprots 用于对外输出
 exprots.a =12;
 exprots.b = 5;
 
-//module
+//module  其中的exprots就是上面exprots引用。  
+module.exprots = {
+a= 10 ,
+b =9,
+show(){}
+
+};
 
 
-//require
+
+
+//require用于引用其它模块
+let a = require("mod2");
+
+
+
 });
 
 
