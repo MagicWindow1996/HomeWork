4c56a1c0b9dec3f5d3b51d31abe35e9f9c989b2d
Linkincc <1751289712@qq.com>
2017-12-16 10:08:11 +0800

    seajs

---
+++ resource/seajs/mod1.js
@@ -0,0 +1,16 @@
+//sea 的模块化必须包含在一个define 中
+define( function(require, exprots,module) {
+    //exprots
+exprots.a =12;
+exprots.b = 5;
+
+//module
+
+
+//require
+});
+
+
+let a = 12;//  直接定义出不去的
+
+let b = 5;
